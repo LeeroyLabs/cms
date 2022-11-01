caec31107c3aecb9a64bd771897f35fde9eb2b08
Philippe <philippe@leeroy.ca>
2022-11-01 13:27:00 -0400

    Comment old code

--- containers/page/Controllers/Page.php
+++ containers/page/Controllers/Page.php
@@ -19,35 +19,36 @@ class Page extends AppController
     public function home():void
     {
         $error = '';
-        $pageEntryQs = new EntryType();
+//        $pageEntryQs = new EntryType();
 
         // It's only for test purpose we can use getByHandle when it's created instead
-        try
-        {
-            $pageEntryType = $pageEntryQs->getOrCreateByHandle(new Collection([
-                'handle' => 'page',
-                'title' => 'Page',
-                'urlPrefix' => ''
-            ]));
-        }
-        catch (DatabaseException|EntryException $exception)
-        {
-            $error = 'There was an error when creating the entry type:' . PHP_EOL . $exception->getMessage() . PHP_EOL;
-        }
-
-        if (isset($pageEntryType)) {
-            $pageQs = new Entry($pageEntryType);
-
-            // It's only for test purpose, when it's created we can use the getBySlug method
-            $pageEntry = $pageQs->getOrCreate(new Collection([
-                'title' => 'Home page',
-                'slug' => '',
-                'status' => EntryStatus::LIVE
-            ]));
-            $this->response->set('title', $pageEntry->title);
-        }
+//        try
+//        {
+//            $pageEntryType = $pageEntryQs->getOrCreateByHandle(new Collection([
+//                'handle' => 'page',
+//                'title' => 'Page',
+//                'urlPrefix' => ''
+//            ]));
+//        }
+//        catch (DatabaseException|EntryException $exception)
+//        {
+//            $error = 'There was an error when creating the entry type:' . PHP_EOL . $exception->getMessage() . PHP_EOL;
+//        }
+//
+//        if (isset($pageEntryType)) {
+//            $pageQs = new Entry($pageEntryType);
+//
+//            // It's only for test purpose, when it's created we can use the getBySlug method
+//            $pageEntry = $pageQs->getOrCreate(new Collection([
+//                'title' => 'Home page',
+//                'slug' => '',
+//                'status' => EntryStatus::LIVE
+//            ]));
+//            $this->response->set('title', $pageEntry->title);
+//        }
 
         $this->response->set('error', $error);
+
         $this->response->template = 'default/page';
     }
 }
